96d8f2a2a366ee539dd57245ed6c768058eac701
meRveet <mervintee@gmail.com>
2020-11-07 21:18:17 +0800

    final data

--- run_analysis.R
+++ run_analysis.R
@@ -5,6 +5,7 @@
 # 3. Uses descriptive activity names to name the activities in the data set
 # 4. Appropriately labels the data set with descriptive variable names.
 # 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+library(mgsub, plyr, data.table, stats)
 
 #reading values from training folder
 xtrain<- read.table("./train/X_train.txt")
@@ -37,16 +38,31 @@ names(xtest)<- tolower(gsub(",", "" , names(xtest)))
 #labeling colnames for activity label list
 colnames(labels)<- c("activityid", "activitylabel")
 
+###(1)###
 #merging all data sets
 mtrain<- cbind(subtrain, ytrain, xtrain)
 mtest<- cbind(subtest, ytest,xtest)
 all<- rbind(mtrain,mtestt)
 
+###(2)###
 #extracting columns with mean data for each variable using grep to call for column number to subset columns into meandata 
 # "|" is used to search up  
 meandata<- all[,grep("subid|activityid|mean|std" , names(all))]
 
-#matching activityid ot activity labels 
-withlabels<- merge(all, labels,by = "activityid", all.x = T)
+###(3)###
+#matching activityid to activity labels 
+tidydata<- merge(meandata, labels,by = "activityid", all.x = T)
 
-aggregate()
+###(4)###
+#substituting variables names with tidy names
+x<- c("acc", "gyro", "bodybody", "mag", "^t", "^f", "tbody", "-mean\\(\\)", "-std\\(\\)", "-freq\\(\\)", "angle", "gravity")
+y<- c("accelerometer", "gyroscope", "body", "magnitude", "time", "frequency", "timebody", "mean", "std", "frequency", "angle", "gravity")
+names(tidydata) <- mgsub(names(tidydata), pattern = x, replacement =  y)
+
+###(5)###
+#summarising data by grouping according ot subject id and activity of each subject with average of each feature 
+finaldata<- tidydata %>% 
+                group_by(subid, activitylabel) %>%
+                summarise_all(funs(mean))
+
+write.table(finaldata, "finaldata.txt, row.name=F")
